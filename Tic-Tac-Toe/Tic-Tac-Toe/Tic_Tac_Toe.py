31d026c870d79a6b537309c52bac34748604331a
Blick913 <94084594+Blick1013@users.noreply.github.com>
2023-05-18 17:28:53 -0400

    First Push

--- Tic-Tac-Toe/Tic-Tac-Toe/Tic_Tac_Toe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/Tic_Tac_Toe.py
@@ -1,2 +1,15 @@
-def is_board(board)
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def is_winner(board, player):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] == player or \
+            board[0][i] == board[1][i] == board[2][i] == player:
+            return True
+        if board[0][0] == board[1][1] == board[2][2] == player or \
+            board[0][2] == board[1][1] == board[2][0] == player:
+            return True
+        return False
 
